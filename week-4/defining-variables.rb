5e769e9c1652a2c242d374f379b2b2d49a807ff8
gaw1990 <georgewambold@gmail.com>
2015-09-08 15:47:58 -0500

    add reflection

--- week-4/defining-variables.rb
+++ week-4/defining-variables.rb
@@ -39,3 +39,45 @@ describe 'age' do
     expect(age).to be_a Fixnum
   end
 end
+
+=begin
+
+1. How do you define a local variable?
+
+ Variable_Name = ____(string, integer or boolean value) 
+
+2. How do you define a method?
+
+ def Method_name(variable 1, variable 2, variable 3)
+ (argument)
+ end 
+
+3. What is the difference between a local variable and a method?
+
+A method can take multiple variables and return an output. A local variable is a static value for the session. 
+
+4. How do you run a ruby program from the command line?
+
+ruby your_ruby_file.rb
+
+
+5. How do you run an RSpec file from the command line?
+rspec your_rspec_file.rb
+
+
+
+
+6. What was confusing about this material? What made sense?
+Nothing was confusing, though my 4.3.1 method wasn't passing the rspec for a while.
+It turned out that using puts outputs 'nil' so even though I was seeing the correct
+output being printed to the console, the rspec file was expecting a RETURN of that
+value. I ultimately had to use 'p' instead of 'puts' and it worked. That took me
+a significant amount of time to figure out though.
+
+
+=end
+
+
+
+
+
