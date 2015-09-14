20a3d125375970250835093d57e1e52f57969764
gaw1990 <georgewambold@gmail.com>
2015-09-14 00:01:04 -0500

    4.7.2

--- week-4/4.7/4.7.2/my_solution.rb
+++ week-4/4.7/4.7.2/my_solution.rb
@@ -9,12 +9,24 @@
 # 0. total Pseudocode
 # make sure all pseudocode is commented out!
 
-# Input:
-# Output:
-# Steps to solve the problem.
+# Input: array
+# Output: sum of all objects (integers)
+# Steps to solve the problem: Stop the method if there isn't anything in the array, add all elements, return the product.
 
 
 # 1. total initial solution
+
+#def total(array_1)
+  #if array_1.empty?
+   # return 0
+  #else 
+   # array_1.inject(0) {|sum, n| sum+n}
+   #end
+   #return sum
+  #end
+#end
+
+# 3. total refactored solution
 
 def total(array_1)
   if array_1.empty?
@@ -28,24 +40,12 @@ def total(array_1)
   end
 end
 
-# 3. total refactored solution
-
-#def total(array_1)
-  #if array_1.empty?
-   # return 0
-  #else 
-   # array_1.inject(0) {|sum, n| sum+n}
-   #end
-   #return sum
-  #end
-#end
-
 
 # 4. sentence_maker pseudocode
 # make sure all pseudocode is commented out!
-# Input:
-# Output:
-# Steps to solve the problem.
+# Input: array of strings
+# Output: array with a single string who's first letter is capitalized and who has a period on the end
+# Steps to solve the problem: capitalize the first string, join the strings, push a period, get rid of the space
 
 
 # 5. sentence_maker initial solution
