b42aea54ba623e5ffa09bfd2a3a8e4accde9d073
gaw1990 <georgewambold@gmail.com>
2015-09-21 17:13:13 -0500

    gps 3.2 NO REFLECTION

--- 6-week/gps2.3/gps.rb
+++ 6-week/gps2.3/gps.rb
@@ -6,31 +6,28 @@
 
 # Bakery Serving Size portion calculator.
 
-def serving_size_calc(item_to_make, order_quantity)
-  library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
-
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      p error_counter += -1
-    end
-  end
-
-  if error_counter > 0
+def serving_size_calc(item_to_make, order_quantity) #method with two arguments
+  library = {"cookie" => 1, "cake" =>  5, "pie" => 7} #hash
+  
+  unless library.include?(item_to_make)
     raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
-
-  serving_size = library.values_at(item_to_make)[0]
+  end #unless
+  
+  serving_size = library[item_to_make]
   serving_size_mod = order_quantity % serving_size
 
-  case serving_size_mod
-  when 0
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
-  else
-    return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
-  end
-end
+    if serving_size_mod == 0
+      return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}"
+    elsif serving_size_mod >= 7
+      return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredient(s). You can make #{serving_size_mod/7} more pie(s) plus #{serving_size_mod % 7} more cookies."
+    elsif serving_size_mod >= 5 
+      return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredient(s). You can make #{serving_size_mod/5} more cake(s) plus #{serving_size_mod % 5} more cookies."
+    else 
+      return "Calculations complete: Make #{order_quantity/serving_size} of #{item_to_make}, you have #{serving_size_mod} leftover ingredient(s). You can have #{serving_size_mod/1} more cookie(s)."
+    end #if  
 
+end #def
+  
 p serving_size_calc("pie", 7)
 p serving_size_calc("pie", 8)
 p serving_size_calc("cake", 5)
@@ -40,4 +37,3 @@ p serving_size_calc("cookie", 10)
 p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
-
