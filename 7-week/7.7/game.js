cc19e094a6d04fa3d37bac94f12dfa3ef480a3ba
gaw1990 <georgewambold@gmail.com>
2015-10-01 16:55:08 -0500

    start prototyping game

---
+++ 7-week/7.7/game.js
@@ -0,0 +1,66 @@
+// Welcome to Belle Plaine 
+
+//  Game ideas:
+// Oh my god.. It's first of the month! You have to get the rent check to your evil landlord before they 
+// charge you fees, evict you, or worse.. Make you move to the garden unit!
+// 
+// 
+//
+//
+// enemy 1: shitty upstairs neighbors
+// 
+// if you take a shortcut through the CVS parking lot
+// enemy 2: bycicle kids in the CVS parking lot! 
+// 
+// final boss-- evil landlord
+// 
+// 
+// pseudocode
+//
+// var max_health = 100
+// var health = 100
+//
+// var strength = 5
+//
+// var max_energy = 10
+// var energy = 8
+//
+// var name = prompt("what's your name?")
+// var start = alert("Ok, " + name + " ready to play 'Belle Plaine'?")
+// 		IF 
+//
+// print out-- "It's 8:30am on a sunny, beautiful Thursday morning in North Center, Chicago" 
+// print out-- "Bees are buzzing, children are playing and birds are chirping"
+// print out-- "..Hmm, actually these birds sound kind of funny-- Like they're almost beeping"
+// print out-- "beeeep, beeeep, beeeep"
+// print out-- "Wait.. Those aren't birds.... It's your alarm clock!!"
+// print out-- "After a long night of DBC work, you're still exausted!! Your energy is at a " + energy + " out of " + max_energy
+// snoozeResponse = prompt("Type 'snooze' to sleep another half hour and gain back 2 energy or type 'off' to turn off the alarm and get up")
+// IF snoozeResponse === "snooze" then 
+	//print out "You slam your alarm (hoping you hit the snooze button) and drift back off to sleep"
+	// print out-- "zzzzzzz"
+	// print out-- "zzzzzzz"
+	// print out-- "zzzzzzz"
+	// energy = energy + 2
+	// print out-- "you gain +2 energy"
+	// print out-- "BEEEEP BEEEEP BEEEEP... Damn that's loud, ok now it's 9am and your energy is " + energy + " out of " + max_energy
+	// print out-- "It's time to get up... "
+	// end
+//
+// print out-- "You clean up, start cooking breakfast just like every morning when suddenly you realize.."
+// alert(IT'S OCTOBER 1st!!!!! THE RENT'S DUE)
+// print out-- "you were so busy thinking about crypto-anarchy articles from the 90s that you forgot rent was due today!!!!"
+// print out-- "You grab your rent check and your hoodie and run out the door"
+// print out-- "suddenly you hear 'Hey.. Where do you think YOU'RE going??"
+// print out-- "..it's your shitty upstairs neighbor.."
+// print out-- "'It's a weeknight and we're having a bunch of random people over later... Around 1am....'"
+// print out-- "'I'm going to need your rent money to buy booze, illicit drugs and new bass speakers for the party'"
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
